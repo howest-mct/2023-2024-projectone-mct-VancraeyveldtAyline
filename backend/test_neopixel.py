0d4e0167c62b600228f5ffd829a3435a26edccd9
Ayline Vancraeyveldt <ayline.vancraeyveldt@student.howest.be>
2024-05-28 15:59:48 +0200

    bezig met neopixel

---
+++ backend/test_neopixel.py
@@ -0,0 +1,39 @@
+import time
+from rpi_ws281x import PixelStrip, Color
+
+# LED strip configuratie:
+LED_COUNT = 24       # Aantal LED pixels.
+LED_PIN = 18         # GPIO pin verbonden met de pixels (moet overeenkomen met de gekozen pin).
+LED_FREQ_HZ = 800000 # LED signaal frequentie in hertz (meestal 800kHz)
+LED_DMA = 10         # DMA kanaal om aan te sturen (moet 10 zijn)
+LED_BRIGHTNESS = 255 # Helderheid van de LED's (0-255)
+LED_INVERT = False   # Invert het signaal (True of False)
+LED_CHANNEL = 0      # Kanaal (moet 0 of 1 zijn)
+
+# Initialiseer de LED strip
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+try:
+    print('Druk op Ctrl-C om te stoppen')
+    while True:
+        # Draai een blauwe kleur door de LED's
+        colorWipe(strip, Color(0, 0, 255))
+        time.sleep(1)
+        # Draai een groene kleur door de LED's
+        colorWipe(strip, Color(0, 255, 0))
+        time.sleep(1)
+        # Draai een rode kleur door de LED's
+        colorWipe(strip, Color(255, 0, 0))
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    # Clear de LED strip bij het afsluiten
+    colorWipe(strip, Color(0, 0, 0), 10)
--- backend/voorlopige_hardware.py
+++ backend/voorlopige_hardware.py
@@ -1,12 +1,16 @@
 import time
 from RPi import GPIO
 
-# Klassen
 from mcp3008 import MCP3008
 mcp3008 = MCP3008()
 from btn_handler import ButtonHandler
 button_handler = ButtonHandler(18)
+from lcd import LCD_Display
+from subprocess import check_output
+lcd = LCD_Display(RS=21, E=20, data_pins=[16, 12, 25, 24, 23, 26, 19, 13])
 
+
+buzzer = 22
 btn_joy = 6
 aantal_joystick_ingedrukt = 0
 
@@ -19,6 +23,24 @@ def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(btn_joy, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(btn_joy, GPIO.FALLING, callback=callback_btn_joy, bouncetime=300)
+    GPIO.setup(buzzer, GPIO.OUT)
+
+def play_tone(buzzer):
+    cycles = 5
+    
+    for _ in range(cycles):
+        GPIO.output(buzzer, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(buzzer, GPIO.LOW)
+        time.sleep(0.1)
+
+def get_ip_address():
+    ips = check_output(['hostname', '--all-ip-addresses'])
+    ip_address = ""
+    for byte in ips:
+        ip_address += chr(byte)
+    return ip_address
+
 
 try:
         setup()
@@ -26,9 +48,9 @@ try:
             waarde_joyx = mcp3008.read_channel(0)
             waarde_joyy = mcp3008.read_channel(1)
             waarde_licht = mcp3008.read_channel(2)
-            # print("Waarde op kanaal 0: {}".format(waarde_joyx))
-            # print("Waarde op kanaal 1: {}".format(waarde_joyy))
-            # print("Waarde op kanaal 2: {}".format(waarde_licht))
+            print(get_ip_address)
+            lcd.send_text(get_ip_address())
+            play_tone(buzzer)
             time.sleep(1) 
 except KeyboardInterrupt:
     mcp3008.close()
